7c1efbbb5588a0bb295cce5ef0cc3a5ee6ebefee
Casey Driscoll <caseypatrickdriscoll@me.com>
2021-04-20 16:15:32 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">chart</h1>
+      <h1 class="title">charts</h1>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
