c9d2557ca513240a75c4e62c1b620cdd55ce3e89
Casey Driscoll <caseypatrickdriscoll@me.com>
2021-04-20 21:28:44 -0400

    Removes extra test content

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <Nuxt />
-    chart
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">chartsss</h1>
+      <h1 class="title">chart</h1>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
